73775291183bce9fdd9acc42c1c47bc336fe1312
Isaac Carabott <a1926322@adelaide.edu.au>
2024-07-24 12:49:53 +0930

    finished function1-4-.cpp

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -4,8 +4,8 @@
 // but will be implemented somewhere else
 extern int num_count(int[], int, int);
 int main() {
-  int nums[] = {1, 3, 2};
-  int nums_count = 3;
+  int nums[] = {1, 3, 2, 4, 1, 5, 1, 6, 7, 1, 2};
+  int nums_count = 11;
   int to_match = 1;
 
   std::cout << to_match << " appears " << num_count(nums, nums_count, to_match)
