28690de4c6832a722c2f585f0d697b3a16c15029
Isaac Carabott <a1926322@adelaide.edu.au>
2024-07-24 13:55:06 +0930

    finished function-2-4.cpp

---
+++ function-2-4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+bool is_ascending(int array[], int n){
+    if (n < 0){
+        return false;
+    }
+    
+    bool array_is_ascending = true;
+
+    for (int i = 1; i < n; i++){
+        if (array[i] < array[i-1]){
+            array_is_ascending = false;
+            std::cout << i << " " << array[i] << " " << array[i-1] << std::endl;
+        } 
+    }
+    return array_is_ascending;
+}
---
+++ main-2-4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// The `extern` keyword tells the compiler that the count function exists,
+// but will be implemented somewhere else
+extern int is_ascending(int[], int);
+
+int main() {
+  int nums[] = {3, 24, 5};
+  int nums_length = 3;
+
+  if (is_ascending(nums, nums_length)) {
+    std::cout << "Array is ascending" << std::endl;
+  } else {
+    std::cout << "Array is descending" << std::endl;
+  }
+
+  return 0;
+}
