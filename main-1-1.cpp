7e16f6970a87a29a5876a4a6e7c5b158471f6c3d
Isaac Carabott <a1926322@adelaide.edu.au>
2024-07-24 12:28:49 +0930

    finished function-1-1.cpp

---
+++ main-1-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// The `extern` keyword tells the compiler that the count function exists,
+// but will be implemented somewhere else
+extern int array_sum(int[], int);
+
+int main(){
+
+    int nums[] = {1, 2, 3, 4, 5};
+
+    std::cout << "Sum of array is " << array_sum(nums, 5) << std::endl;
+
+    return 0;
+}
