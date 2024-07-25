2fc89f5ed056af0c152b2986faaeeb0188b6ddd9
Isaac Carabott <a1926322@adelaide.edu.au>
2024-07-25 13:49:34 +0930

    testing function-2-4.cpp, shouldnt be broken but grader says it is v2

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 bool is_ascending(int array[], int n){
+    
+    std::cout << "Array is {";
+    for (int i = 0; i < n; i++){
+        std::cout << array[i];
+        if (i+1 < n){
+            std::cout << ", ";
+        }
+    }
+    std::cout << "}" << std::endl;
+    
     if (n < 0){
         return false;
     }
@@ -10,7 +20,8 @@ bool is_ascending(int array[], int n){
     for (int i = 1; i < n; i++){
         if (!(array[i] >= array[i-1])){
             array_is_ascending = false;
-            std::cout << "i" << i << " a[i-1]" << array[i-1] << " a[i]" << array[i] << std::endl;
+            std::cout << "i" << i << " a[i-1]" << array[i-1] << " a[i]" << array[i] << " comp" << !(array[i] >= array[i-1]) << " asc" << array_is_ascending <<std::endl;
+            break;
         } 
     }
     return array_is_ascending;
