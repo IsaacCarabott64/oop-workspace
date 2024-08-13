e5e31655a448c7f7bfad814818f5021890722414
Isaac Carabott <a1926322@adelaide.edu.au>
2024-08-13 17:29:45 +0930

    fixed function-1-2

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -4,7 +4,7 @@ PersonList createPersonList(int n){
   PersonList plist;
   plist.people = new Person[n];
   for (int i = 0; i < n; i++){
-    plist.people[i].name = "John Doe";
+    plist.people[i].name = "Jane Doe";
     plist.people[i].age = 1;
   }
   plist.numPeople = n;
