9534be239ff5db9e7e385fefd60ec45dd6513332
Isaac Carabott <a1926322@adelaide.edu.au>
2024-08-24 04:31:45 +0930

    doing stuff with ssh

--- student.h
+++ student.h
@@ -2,15 +2,15 @@
 
 
 class Student {
- public:
-  string name;
-  int ID;
-  double grade;
-  Student(string s_name, int s_ID, double s_grade) {
-    name = s_name;
-    ID = s_ID;
-    grade = s_grade;
-  }
+  public:
+    string name;
+    int ID;
+    double grade;
+    Student(string s_name, int s_ID, double s_grade) {
+      name = s_name;
+      ID = s_ID;
+      grade = s_grade;
+    }
   string get_name() { return name; }  // behaviour
   int get_ID() { return ID; }
   double get_grade() { return grade; }
