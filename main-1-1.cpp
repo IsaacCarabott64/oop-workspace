cf6aab306a1aeb22238b2dc83fbd6331eefd32b7
Isaac Carabott <a1926322@adelaide.edu.au>
2024-08-30 19:17:59 +0930

    testing

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -8,17 +8,6 @@ using namespace std;
 
 int main(){
 
-  int n = 3;  
-
-  Vehicle** vehicles = new;
-  
-  vehicles[0] = new Car(1);
-  vehicles[1] = new Bus(2);
-  vehicles[2] = new Motorbike(3);
-
-  for (int i = 0; i < n; i++){
-    cout << "Vehicle: " << vehicles[i]->getID() << " ParkingDuration: " << vehicles[i]->getParkingDuration() << endl;
-  }
 
 
   return 0;
