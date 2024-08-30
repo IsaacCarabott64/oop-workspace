b118a72546a683f9272d3ba0d125836730f90e5a
Isaac Carabott <a1926322@adelaide.edu.au>
2024-08-30 19:26:37 +0930

    testing

--- Bus.cpp
+++ Bus.cpp
@@ -4,5 +4,5 @@
 Bus::Bus(int ID): Vehicle(ID){};
 
 int Bus::getParkingDuration(
-  return (int)((time(nullptr) - Vehicle::getTimeOfEntry)*0.85);
+  return (int)((time(nullptr) - Vehicle::getTimeOfEntry)*0.75);
 );
