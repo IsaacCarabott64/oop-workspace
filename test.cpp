be865c4a6fb20849ad04f23d2b3f10d47e78b8a7
Isaac Carabott <a1926322@adelaide.edu.au>
2024-09-11 01:22:08 +0930

    7-1

--- test.cpp
+++ test.cpp
@@ -2,71 +2,118 @@
 
 using namespace std;
 
-
-class Point{
-  private:
-    int* _x;
-    int* _y;
+class Printable {
   public:
-    Point() {
-      _x = new int;
-      _y = new int;
-      *_x = 0;
-      *_y = 0;
-    }
-    Point(const Point &pnt){
-      _x = new int;
-      _y = new int;
-      *_x = *(pnt._x);
-      *_y = *(pnt._y);
-
-    }
-    void operator=(const Point &pnt){
-      *_x = *(pnt._x);
-      *_y = *(pnt._y);
-    }
-    void set_point(int x, int y){ *_x = x; *_y = y; }
-    void print(){
-      cout << *_x << "," << *_y << endl;
-    }
-    virtual ~Point(){
-      delete _x;
-      delete _y;
-      cout << "_x and _y were deleted" << endl;
-    }
-
+    virtual void print() = 0;
 };
 
-class Point3d : public Point {
-  private:
-    int* _z;
-  public:
-    Point3d():Point(){
-      _z = new int;
-    }
-    void set_point(int x, int y, int z){
-      Point::set_point(x, y);
-      *_z = z;
-    }
-    ~Point3d(){
-      delete _z;
-      cout << "_z was deleted\n";
-    }
+struct Point {
+  double x;
+  double y;
 };
 
-Point foo(){
-  Point pnt;
-  pnt.set_point(500, 500);
-  return pnt;
+class Shape : public Printable {
+  protected:
+    int n_sides;
+    Point center;
+  public:
+    Shape(double x, double y){
+      center.x = y;
+      center.y = x;
+      n_sides = 0;
+    }
+    Shape(): Shape(0, 0){}
+    double get_sides(){ return n_sides; }
+    virtual double get_area() = 0;
+};
+
+class Square : public Shape {
+  private:
+    double _side;
+  public:
+    Square(double cx, double cy, double side){
+      center.x = cx;
+      center.y = cy;
+      _side = side;
+      n_sides = 4;
+    }
+    Square(): Shape(),_side(0){}
+    double get_area(){
+      return _side * _side;
+    }
+    void print(){ cout << "This is a square with side length " << _side << endl; }
+};
+
+class Triangle : public Shape {
+  private:
+    double _base;
+    double _height;
+  public:
+    Triangle(double cx, double cy, double b, double h){
+      center.x = cx;
+      center.y = cy;
+      _base = b;
+      _height = h;
+      n_sides = 3; 
+    }
+    Triangle(): Shape(),_base(0),_height(0){}
+    double get_area(){
+      return 0.5 * _base * _height;
+    }
+    void print(){ cout << "This is a triangle with base " << _base << " and height " << _height << endl; }
+};
+
+class Circle : public Shape {
+  private:
+    double _radius;
+  public:
+    Circle(double cx, double cy, double radius){
+      this->center.x = cx;
+      this->center.y = cy;
+      this->_radius = radius;
+    }
+    Circle(): Shape(),_radius(0){}
+    double get_area(){
+      return 3.14 * _radius * _radius;
+    }
+    void print(){ cout << "This is a circle with radius " << _radius << endl; }
+};
+
+void test(Shape* s){
+  cout << "Shape (" << s->get_sides() << ") has area " << s->get_area() << endl;
 }
 
+class Person : public Printable{
+  private:
+    string _name;
+  public:
+    Person(string name): _name(name){}
+    Person(): Person("null"){}
+    string get_name(){ return _name; }
+    void set_name(string name){this->_name = name;}
+    void print(){ cout << "This is a person named " << _name << endl; }
+};
 
+void print_summary(Printable* pobj){
+  pobj->print();
+};
 
 int main(){
 
-  Point* p = new Point3d;
+  Shape* sq = new Square(0, 0, 3);
+  Shape* tr = new Triangle(0, 0, 4, 8);
+  Shape* ci = new Circle(0, 0, 2);
 
-  delete p;
+  test(sq);
+  test(tr);
+  test(ci);
+
+  Person* pe = new Person("Isaac");
+
+  print_summary(sq);
+  print_summary(tr);
+  print_summary(ci);
+  print_summary(pe);
 
   return 0;
 }
