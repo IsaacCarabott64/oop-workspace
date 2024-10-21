33bdfd577e6f3d9aaec93ae81252c33e454297cf
Isaac Carabott <a1926322@adelaide.edu.au>
2024-10-21 20:48:55 +1030

    fixed main-1

--- main-1.cpp
+++ main-1.cpp
@@ -5,20 +5,18 @@
 
 using namespace std;
 
- 
-
 
 int main(){
 
   srand(time(NULL));
 
   GameEntity ge(1, 2, 'E');
-  cout << "Type: " << ge.getType() << "   Pos: " << tupleToStr(ge.getPos()) << endl;
+  cout << "Type: " << ge.getType() << "   Pos: " << Utils::tupleToStr(ge.getPos()) << endl;
 
   for (int i = 0; i < 10; i++){
     tuple<int,int> p1 = Utils::generateRandomPos(10,10);
     tuple<int,int> p2 = Utils::generateRandomPos(10,10);
-    cout << "Pos 1: " << tupleToStr(p1) << "  Pos 2:" << tupleToStr(p2) << "   Dist: " << Utils::calculateDistance(p1, p2) << endl;
+    cout << "Pos 1: " << Utils::tupleToStr(p1) << "  Pos 2:" << Utils::tupleToStr(p2) << "   Dist: " << Utils::calculateDistance(p1, p2) << endl;
   }
 
   
