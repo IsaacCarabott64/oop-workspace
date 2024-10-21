5c70a16c441dd2a1a83427181107a2a4bc79aabf
Isaac Carabott <a1926322@adelaide.edu.au>
2024-10-21 21:34:54 +1030

    finished q3 fr

--- Game.h
+++ Game.h
@@ -22,13 +22,13 @@ class Game{
       for (int i = 0; i < numShips; i++){
         tuple<int,int> newPos = Utils::generateRandomPos(gridWidth, gridHeight);
         Ship* newShip = new Ship(get<0>(newPos), get<1>(newPos));
-        newShip->print();
+        //newShip->print();
         entities.push_back(newShip);
       }
       for (int i = 0; i < numMines; i++){
         tuple<int,int> newPos = Utils::generateRandomPos(gridWidth, gridHeight);
         Mine* newMine = new Mine(get<0>(newPos), get<1>(newPos));
-        newMine->print();
+        //newMine->print();
         entities.push_back(newMine);
       }
 
@@ -37,22 +37,22 @@ class Game{
 
     void gameLoop(int maxIterations, double mineDistanceThreshold){
       for (int i = 0; i < maxIterations; i++){
-        cout << "\nRound " << i+1 << endl;
+        //cout << "\nRound " << i+1 << endl;
 
         // for each ship
         for (int s = 0; s < (int)entities.size(); s++){
           if (entities[s]->getType() == GameEntityType::ShipType){
-            entities[s]->print();
+            //entities[s]->print();
             ((Ship*)entities[s])->move(1, 0);
 
             // for each mine
             for (int m = 0; m < (int)entities.size(); m++){
               if (entities[m]->getType() == GameEntityType::MineType){
-                entities[m]->print();
+                //entities[m]->print();
 
                 // if distance to mine is less than threshold
                 if (Utils::calculateDistance(entities[s]->getPos(), entities[m]->getPos()) <= mineDistanceThreshold){
-                  cout << "EXPLOSION!!!" << endl;
+                  //cout << "EXPLOSION!!!" << endl;
                   Explosion e = ((Mine*)entities[m])->explode();
                   e.apply(*entities[s]);
                 }
